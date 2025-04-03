c310fc993028a981cd34b63dd5191f9beb6f5710
marmooo <marmooo@users.noreply.github.com>
2025-04-04 00:51:13 +0900

    bump @marmooo/soundfont-parser from 0.0.2 to 0.0.3

--- src/midy.js
+++ src/midy.js
@@ -2,7 +2,7 @@ import { parseMidi } from "https://cdn.jsdelivr.net/npm/midi-file@1.2.4/+esm";
 import {
   parse,
   SoundFont,
-} from "https://cdn.jsdelivr.net/npm/@marmooo/soundfont-parser@0.0.2/+esm";
+} from "https://cdn.jsdelivr.net/npm/@marmooo/soundfont-parser@0.0.3/+esm";
 
 class Note {
   bufferSource;
