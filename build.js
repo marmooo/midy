8f591c544153ea1e1ddb01c221a27fed225c52b4
marmooo <marmooo@users.noreply.github.com>
2025-05-27 03:28:20 +0900

    add non-optimized build

--- build.js
+++ build.js
@@ -1,25 +1,33 @@
 import { minify, transform } from "@swc/core";
 
-async function buildScript(inPath, outPath) {
+async function buildScript(inPath, outPath, optimize) {
   const code = Deno.readTextFileSync(inPath);
   const transformed = await transform(code, {
     isModule: true,
     sourceMaps: false,
   });
-  const minified = await minify(transformed.code, {
-    compress: true,
-    mangle: true,
-    module: true,
-    sourceMap: false,
-  });
-  Deno.writeTextFileSync(outPath, minified.code);
+  if (optimize) {
+    const minified = await minify(transformed.code, {
+      compress: optimize,
+      mangle: optimize,
+      module: true,
+      sourceMap: false,
+    });
+    Deno.writeTextFileSync(outPath, minified.code);
+  } else {
+    Deno.writeTextFileSync(outPath, transformed.code);
+  }
 }
 
 async function build() {
-  await buildScript("./src/midy.js", "./dist/midy.min.js");
-  await buildScript("./src/midy-GM2.js", "./dist/midy-GM2.min.js");
-  await buildScript("./src/midy-GM1.js", "./dist/midy-GM1.min.js");
-  await buildScript("./src/midy-GMLite.js", "./dist/midy-GMLite.min.js");
+  await buildScript("./src/midy.js", "./dist/midy.js", false);
+  await buildScript("./src/midy-GM2.js", "./dist/midy-GM2.js", false);
+  await buildScript("./src/midy-GM1.js", "./dist/midy-GM1.js", false);
+  await buildScript("./src/midy-GMLite.js", "./dist/midy-GMLite.js", false);
+  await buildScript("./src/midy.js", "./dist/midy.min.js", true);
+  await buildScript("./src/midy-GM2.js", "./dist/midy-GM2.min.js", true);
+  await buildScript("./src/midy-GM1.js", "./dist/midy-GM1.min.js", true);
+  await buildScript("./src/midy-GMLite.js", "./dist/midy-GMLite.min.js", true);
 }
 
 await build();
