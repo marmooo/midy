70ec53463765328b4418748a5a25b7e220d870cd
marmooo <marmooo@users.noreply.github.com>
2026-06-14 05:03:54 +0900

    add playback tests

--- tests/GM1-mock/mod.ts
+++ tests/GM1-mock/mod.ts
@@ -5,6 +5,7 @@ import {
   registerChannelTests,
   registerNoteTests,
   registerPedalTests,
+  registerPlaybackTests,
   registerRPNTests,
   registerTimelineTests,
 } from "../basic-mock/mod.ts";
@@ -17,3 +18,4 @@ registerPedalTests(factory, label);
 registerChannelTests(factory, label);
 registerRPNTests(factory, label);
 registerTimelineTests(factory, label);
+registerPlaybackTests(factory, label);
